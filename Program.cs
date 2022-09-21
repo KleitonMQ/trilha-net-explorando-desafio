133a955d1024bc694df67c3628db8e3f89a52b58
Kleiton MQ <kleiton.x5@gmail.com>
2022-09-21 16:56:10 -0300

    conclusao do projeto de desafio proposto pela DIO no bootcamp da Pottencial.

--- Program.cs
+++ Program.cs
@@ -5,20 +5,34 @@ Console.OutputEncoding = Encoding.UTF8;
 
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
+var quantidadeDeHospedes = 0;
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Console.WriteLine("Informe a quantidade de hospedes.");
+int.TryParse(Console.ReadLine(), out quantidadeDeHospedes);
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+for (int i = 0; i < quantidadeDeHospedes; i++)
+{
+    Console.WriteLine("Insira o nome do hospede.");
+    var nome = Console.ReadLine();
+    Console.WriteLine("Insira o sobrenome do hospede.");
+    var sobrenome = Console.ReadLine();
+    Pessoa pessoa = new Pessoa(nome: nome, sobrenome: sobrenome);
+    hospedes.Add(pessoa);
+}
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+var tipoSuite = "Elite";
+var capacidade = 1;
+var valorDiaria = 60;
+Suite suite = new Suite(tipoSuite: tipoSuite, capacidade: capacidade, valorDiaria: valorDiaria);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+var diasReservados = 30;
+
+Reserva reserva = new Reserva(diasReservados: diasReservados);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
+
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
